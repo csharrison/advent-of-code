4bf5eeaba85cdd36c696999744ec19f666e5de47
Charlie Harrison <csharrison@chromium.org>
2023-12-01 14:54:47 -0600

    solution part 2

--- 2023/1/solution.py
+++ 2023/1/solution.py
@@ -1,7 +1,23 @@
+import string
 def parse_line(line: str):
   digits = [d for d in line if d.isdigit()]
   return int(digits[0] + digits[-1])
 
+DIGIT_WORDS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+def to_digit(digit_or_word: str):
+  if digit_or_word not in DIGIT_WORDS:
+    return digit_or_word
+  return str(DIGIT_WORDS.index(digit_or_word) + 1)
+
+def parse_line2(line: str):
+  to_find = DIGIT_WORDS + list(string.digits[1:])
+  minimum = min((line.index(d), d) for d in to_find if d in line)
+  maximum = max((line.rindex(d), d) for d in to_find if d in line)
+
+  return int(to_digit(minimum[1]) + to_digit(maximum[1]))
+
 if __name__ == "__main__":
   with open('input.txt', 'r') as f:
-    print(sum(parse_line(l) for l in f.readlines()))
+    lines = f.readlines()
+    print(sum(parse_line(l) for l in lines))
+    print(sum(parse_line2(l) for l in lines))
